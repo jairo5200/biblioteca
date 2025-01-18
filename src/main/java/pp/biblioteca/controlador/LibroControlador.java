6e13e91ccfbbf12640f8a86b38555362d539b9d7
jairo5200 <jairo5200@gmail.com>
2025-01-18 11:30:46 -0500

    iimplementamos el metodo buscar libro por id

--- src/main/java/pp/biblioteca/controlador/LibroControlador.java
+++ src/main/java/pp/biblioteca/controlador/LibroControlador.java
@@ -3,7 +3,9 @@ package pp.biblioteca.controlador;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pp.biblioteca.excepcion.RecursoNoEncontradoExcepcion;
 import pp.biblioteca.modelo.Libro;
 import pp.biblioteca.servicio.LibroServicio;
 
@@ -32,5 +34,16 @@ public class LibroControlador {
     public Libro agregarLibro(@RequestBody Libro libro){
         Libro elLibro = this.libroServicio.guardarLibro(libro);
         return elLibro;
+    }
+
+    //http://localhost:8080/biblioteca-app/libros/id
+    @GetMapping("/libros/{id}")
+    public ResponseEntity<Libro> obtenerLibroPorId(@PathVariable int id) {
+        Libro libro = this.libroServicio.buscarLibroPorId(id);
+        if (libro != null) {
+            return ResponseEntity.ok(libro);
+        } else {
+             throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+        }
     }
 }
